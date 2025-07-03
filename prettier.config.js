dd8dd1c43f0793c571bb5ec34ada01baf22d2072
ShovitDutta <shovitdutta1@gmail.com>
2025-07-03 11:15:58 +0530

    Initial commit: Flask webhook receiver with MongoDB

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+/** @type {import("prettier").Config} */
+const prettierConfig = {
+    bracketSameLine: false,
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    singleQuote: false,
+    printWidth: 200,
+    tabWidth: 4,
+    semi: true,
+    endOfLine: "lf",
+    proseWrap: "always",
+    trailingComma: "all",
+    arrowParens: "always",
+    htmlWhitespaceSensitivity: "css",
+    embeddedLanguageFormatting: "auto",
+};
+export default prettierConfig;
